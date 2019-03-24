4b58964abdc12747d839b2d334be4b7d49ab1b6d
墨林津枫 <2777372207@qq.com>
2019-03-24 22:48:14 +0800

    Revert "Revert "更改项目路径、获取模板名""
    This reverts commit 3b9665a13e6034b1620baec91085268cab1ca4e7.

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://127.0.0.1:8082/zsPC';
-$config['base_url'] = 'http://127.0.0.1:8081/zsPC';
+$config['base_url'] = 'http://127.0.0.1:8081/ZS-PC';
 /*
 |--------------------------------------------------------------------------
 | Index File
